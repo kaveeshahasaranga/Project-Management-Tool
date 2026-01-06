d23de27c4a3425f285ce8d371353c832cba0361d
Kaveeshahasaranga <kaveeshahasaranga57@gmail.com>
2026-01-07 02:23:40 +0530

    chore: Install project dependencies by adding various tooling and library files to `node_modules`.

---
+++ kanban-ui/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>My Kanban Board 📋</h1>
+      <p>අපි මේක ලස්සනට හදමු!</p>
+    </div>
+  );
+}
+
+export default App;
